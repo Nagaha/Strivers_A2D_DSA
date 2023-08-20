ca2ccdef4270f952242f59af7e39b2e658af06e7
Harish <121287864+Nagaha@users.noreply.github.com>
2023-08-20 16:07:58 +0530

    Solution for Pattern5 GFG

---
+++ Pattern5.java
@@ -0,0 +1,8 @@
+    void printTriangle(int n) {
+        for(int i=n;i>0;i--){
+            for(int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
