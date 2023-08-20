2dbb136b28fa72aa5b016ed4ab12ef43e90b3dd3
Harish <121287864+Nagaha@users.noreply.github.com>
2023-08-20 16:12:54 +0530

    Solution for pattern 6 GFG

---
+++ Pattern6GFG.java
@@ -0,0 +1,8 @@
+    void printTriangle(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
