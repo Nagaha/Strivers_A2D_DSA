5d0fa2ea9910bfa344f09396cc8ab495df2d71ce
Harish <121287864+Nagaha@users.noreply.github.com>
2023-08-20 15:26:17 +0530

    Data Types problem in Coding ninjas

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+        public static int dataTypes(String type) {
+
+            if(type.equals("Integer")) return 4;
+            else if(type.equals("Long")) return 8;
+            else if(type.equals("Float")) return 4;
+            else if(type.equals("Double")) return 8;
+            else return 1;
+        }
+    }
+
