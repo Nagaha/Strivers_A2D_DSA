f3ece865d73a73cf6a92f3e068bd832157c37dad
Harish <121287864+Nagaha@users.noreply.github.com>
2023-08-22 19:10:39 +0530

    Pattern 8  Inverted *  pyramid coding ninjas

---
+++ Pattern8.java
@@ -0,0 +1,17 @@
+public class Pattern8 {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n*2-2*i-1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
